9aaefa5da13c4847feaa634d4732522f3c55fc77
Apipol Sukgler <apipol@computerlogy.com>
2018-06-27 17:31:26 +0700

    complete common interface (new things: know about buffer read and write

--- about_common_interfaces.go
+++ about_common_interfaces.go
@@ -17,7 +17,7 @@ func aboutCommonInterfaces() {
 		   $ open http://localhost:8080/pkg/io/
 		   $ open http://localhost:8080/pkg/bytes/
 		*/
-
+		in.WriteTo(out)
 		assert(out.String() == "hello world") // get data from the io.Reader to the io.Writer
 	}
 
@@ -26,7 +26,8 @@ func aboutCommonInterfaces() {
 		in.WriteString("hello world")
 
 		out := new(bytes.Buffer)
-
+		in.Truncate(5)
+		out.WriteString(in.String())
 		assert(out.String() == "hello") // duplicate only a portion of the io.Reader
 	}
 }
